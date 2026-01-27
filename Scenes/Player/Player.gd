a75895ad2790703f8d7a602e40a9edb275d20fb8
Igor Correa Trifilio Campos <igorcorreatrifilioc@gmail.com>
2026-01-26 22:37:09 -0300

    implementa destruição de chãos frágeis:  	- colisão agora verifica se a tecla shift está ativa no momento da colisão, e se a energia cinética atual do jogador é maior do que o valor de break_force

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -10,12 +10,16 @@ extends CharacterBody3D
 
 var current_mass: float = 1.0
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var air_kinetic_energy : float
 @onready var state_machine = $StateMachine
 @onready var camera_pivot = $CameraPivot
 
 # Getter para uso nos estados
 var is_grounded: bool:
 	get: return is_on_floor()
+
+var kinetic_energy: float:
+	get: return (current_mass * (velocity.y*velocity.y))/2
 
 func _ready():
 	# Inicializa a máquina de estados
@@ -28,7 +32,8 @@ func _physics_process(delta):
 	# Aplica gravidade considerando a massa
 	if not is_on_floor():
 		velocity.y -= gravity * gravity_scale * delta
-	
+		air_kinetic_energy = kinetic_energy
+	#print("Energia cinetica: ",air_kinetic_energy)
 	# Delega o processamento de física para a máquina de estados
 	state_machine.process_physics(delta)
 	move_and_slide()
@@ -39,7 +44,7 @@ func _input(event):
 
 func get_move_direction() -> Vector3:
 	var input_dir = Input.get_vector("move_left", "move_right", "move_back", "move_foward")
-	print("Input: ", input_dir)  # Debug
+	#print("Input: ", input_dir)  # Debug
 	return (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 func can_jump() -> bool:
